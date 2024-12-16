e126106b8171ad2abdcd9e7cc69db4df05374ab0
unish001 <unishshilpakar01@gmail.com>
2024-12-16 20:54:37 +0545

    first project

---
+++ tsd1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter the two number:\n");
+    scanf("%d%d", &a, &b);
+    c = a + b;
+    Printf("sum of %d and %d is %d", a, b, c);
+    return 0;
+}
